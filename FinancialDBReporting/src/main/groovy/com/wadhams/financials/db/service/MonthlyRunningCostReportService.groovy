7313c1e17678a78ac5ff8f4627e75b9d80f83818
rober <rober@Rob-Molly-Laptop.modem>
2019-12-04 13:41:00 +1000

    SpecificRunningCosts at line level reporting

--- FinancialDBReporting/src/main/groovy/com/wadhams/financials/db/service/MonthlyRunningCostReportService.groovy
+++ FinancialDBReporting/src/main/groovy/com/wadhams/financials/db/service/MonthlyRunningCostReportService.groovy
@@ -12,79 +12,114 @@ class MonthlyRunningCostReportService {
 	CommonReportingService commonReportingService = new CommonReportingService()
 	
 	def execute(PrintWriter pw) {
-		String query = buildQuery()
-		println query
-		println ''
-
-		List<FinancialDTO> financialList = databaseQueryService.buildList(query)
-		
-		report(financialList, pw)
-	}
-	
-	def report(List<FinancialDTO> financialList, PrintWriter pw) {
 		pw.println 'MONTHLY RUNNING COST REPORT'
 		pw.println '---------------------------'
 		pw.println ''
 
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy")
-		NumberFormat nf = NumberFormat.getCurrencyInstance()
-
-		Date today = new Date()
-		BigDecimal monthsPerYear = new BigDecimal('12')
-		BigDecimal daysPerYear = new BigDecimal('365')		
+		String query
+		List<FinancialDTO> financialList
 		
-		ListControlBreak cb = new ListControlBreak(financialList)
-		FinancialDTO current = cb.first()
+		query = buildQuery('SPECIFIC_RUNNING_COST')
+		println query
+		println ''
+		financialList = databaseQueryService.buildList(query)
+		reportSpecificRunningCosts(financialList, pw)
 		
-		while (cb.hasMore()) {
-			String savedRG1 = current.rg1
-			//Heading 1
-			String h1 = "$savedRG1 (Monthly)"
-			pw.println h1
-			int length = h1.size()
-			pw.println "-".padRight(length, '-')
-			
-			//Total
-			BigDecimal reportTotal = BigDecimal.ZERO
-			
-			while (cb.hasMore() && savedRG1 == current.rg1) {
-				String savedAsset = current.asset
-				pw.println "$savedAsset:"
-				while (cb.hasMore() && savedRG1 == current.rg1 && savedAsset == current.asset) {
-					println savedAsset
-					String savedCategory = current.category
-					BigDecimal categoryTotal = BigDecimal.ZERO
-					BigDecimal categoryDays = BigDecimal.ZERO
-					while (cb.hasMore() && savedRG1 == current.rg1 && savedAsset == current.asset && savedCategory == current.category) {
-						println savedCategory
-						categoryTotal = categoryTotal.add(current.amount)
-						int days = current.endDt - current.startDt + 1
-						categoryDays = categoryDays.add(days)
-						current = cb.next()
-					}
-					println "$categoryTotal\t$categoryDays"
-					BigDecimal categoryAverage = categoryTotal.multiply(daysPerYear).divide(monthsPerYear, 2).divide(categoryDays, 2)
-					pw.println "\t$savedCategory ${nf.format(categoryAverage)}"
-					reportTotal = reportTotal.add(categoryAverage)
-				}
-				pw.println ''
-			}
-			pw.println "Monthly Total: ${nf.format(reportTotal)}"
-			pw.println ''
-		}
+		query = buildQuery('ONGOING_RUNNING_COST')
+		println query
+		println ''
+		financialList = databaseQueryService.buildList(query)
+		reportOngoingRunningCosts(financialList, pw)
 		
 		pw.println ''
 		pw.println commonReportingService.horizonalRule
 		pw.println ''
 	}
 	
-	String buildQuery() {
+	def reportSpecificRunningCosts(List<FinancialDTO> financialList, PrintWriter pw) {
+		pw.println 'SPECIFIC RUNNING COSTS (MONTHLY)'
+		pw.println '--------------------------------'
+			
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy")
+		NumberFormat nf = NumberFormat.getCurrencyInstance()
+
+		BigDecimal monthsPerYear = new BigDecimal('12')
+		BigDecimal daysPerYear = new BigDecimal('365')		
+		
+		//Total
+		BigDecimal reportTotal = BigDecimal.ZERO
+			
+		ListControlBreak cb = new ListControlBreak(financialList)
+		FinancialDTO current = cb.first()
+
+		while (cb.hasMore()) {
+			String savedAsset = current.asset
+			pw.println "$savedAsset:"
+			while (cb.hasMore() && savedAsset == current.asset) {
+				int days = current.endDt - current.startDt + 1
+				BigDecimal categoryDays = new BigDecimal(days)
+				BigDecimal categoryAverage = current.amount.multiply(daysPerYear).divide(monthsPerYear, 2).divide(categoryDays, 2)
+				pw.println "\t${current.category} ${nf.format(categoryAverage)} (${nf.format(current.amount)} ${sdf.format(current.startDt)} - ${sdf.format(current.endDt)})"
+				reportTotal = reportTotal.add(categoryAverage)
+				current = cb.next()
+			}
+			pw.println ''
+		}
+		pw.println "Monthly Total: ${nf.format(reportTotal)}"
+		pw.println ''
+	}
+	
+	def reportOngoingRunningCosts(List<FinancialDTO> financialList, PrintWriter pw) {
+		pw.println 'ONGOING RUNNING COSTS (MONTHLY)'
+		pw.println '-------------------------------'
+			
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy")
+		NumberFormat nf = NumberFormat.getCurrencyInstance()
+
+		BigDecimal monthsPerYear = new BigDecimal('12')
+		BigDecimal daysPerYear = new BigDecimal('365')		
+		
+		//Total
+		BigDecimal reportTotal = BigDecimal.ZERO
+			
+		ListControlBreak cb = new ListControlBreak(financialList)
+		FinancialDTO current = cb.first()
+		
+		while (cb.hasMore()) {
+			String savedAsset = current.asset
+			pw.println "$savedAsset:"
+			while (cb.hasMore() && savedAsset == current.asset) {
+				//println savedAsset
+				String savedCategory = current.category
+				BigDecimal categoryTotal = BigDecimal.ZERO
+				BigDecimal categoryDays = BigDecimal.ZERO
+				while (cb.hasMore() && savedAsset == current.asset && savedCategory == current.category) {
+					//println savedCategory
+					categoryTotal = categoryTotal.add(current.amount)
+					int days = current.endDt - current.startDt + 1
+					categoryDays = categoryDays.add(days)
+					current = cb.next()
+				}
+				//println "$categoryTotal\t$categoryDays"
+				BigDecimal categoryAverage = categoryTotal.multiply(daysPerYear).divide(monthsPerYear, 2).divide(categoryDays, 2)
+				pw.println "\t$savedCategory ${nf.format(categoryAverage)}"
+				reportTotal = reportTotal.add(categoryAverage)
+			}
+			pw.println ''
+		}
+		pw.println "Monthly Total: ${nf.format(reportTotal)}"
+		pw.println ''
+	}
+	
+	String buildQuery(String rg1) {
 		StringBuilder sb = new StringBuilder()
 		sb.append("SELECT TRANSACTION_DT as TXN, AMOUNT as AMT, PAYEE, DESCRIPTION as DESC, ASSET, CATEGORY as CAT, SUB_CATEGORY as SUBCAT, START_DT as START, END_DT as END, RPT_GRP_1 as RG1, RPT_GRP_2 as RG2, RPT_GRP_3 as RG3 ")
 		sb.append("FROM FINANCIAL ")
 		sb.append("WHERE START_DT IS NOT NULL ")
-		sb.append("AND RPT_GRP_1 IS NOT NULL ")
-		sb.append("ORDER BY RPT_GRP_1, ASSET, CATEGORY, SUB_CATEGORY")
+		sb.append("AND RPT_GRP_1 = '")
+		sb.append(rg1)
+		sb.append("' ")
+		sb.append("ORDER BY ASSET, CATEGORY")
 		
 		return sb.toString()
 	}
