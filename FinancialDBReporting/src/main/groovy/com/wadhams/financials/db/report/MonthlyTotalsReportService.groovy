f453cd5bc99a12f23fae978861e66e28ab126906
robert.wadhams <robert.wadhams@gmail.com>
2020-08-05 14:21:41 +1000

    Monthly updates

--- FinancialDBReporting/src/main/groovy/com/wadhams/financials/db/report/MonthlyTotalsReportService.groovy
+++ FinancialDBReporting/src/main/groovy/com/wadhams/financials/db/report/MonthlyTotalsReportService.groovy
@@ -19,7 +19,7 @@ class MonthlyTotalsReportService {
 		pw.println '--------------------------------------------------'
 
 		//TODO: dynamic month range
-		List<MonthDateRange> mdrList = [MonthDateRange.Sept2019, MonthDateRange.Oct2019, MonthDateRange.Nov2019, MonthDateRange.Dec2019, MonthDateRange.Jan2020, MonthDateRange.Feb2020, MonthDateRange.Mar2020, MonthDateRange.Apr2020, MonthDateRange.May2020, MonthDateRange.Jun2020]
+		List<MonthDateRange> mdrList = [MonthDateRange.Sept2019, MonthDateRange.Oct2019, MonthDateRange.Nov2019, MonthDateRange.Dec2019, MonthDateRange.Jan2020, MonthDateRange.Feb2020, MonthDateRange.Mar2020, MonthDateRange.Apr2020, MonthDateRange.May2020, MonthDateRange.Jun2020, MonthDateRange.Jul2020]
 		
 		BigDecimal total = new BigDecimal(0.0)
 		
