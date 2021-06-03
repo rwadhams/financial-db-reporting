6970874a9a4f3d50a685ce44fdafd968d9dc1ef0
robert.wadhams <robert.wadhams@gmail.com>
2021-06-03 16:57:20 +1000

    Additional sorting in CategoryTotal report

--- FinancialDBReporting/src/main/groovy/com/wadhams/financials/db/report/CategoryTotalReportService.groovy
+++ FinancialDBReporting/src/main/groovy/com/wadhams/financials/db/report/CategoryTotalReportService.groovy
@@ -14,6 +14,7 @@ class CategoryTotalReportService {
 	CommonReportingService commonReportingService = new CommonReportingService()
 	
 	def execute(PrintWriter pw) {
+		//Report by Category
 		pw.println 'CATEGORY TOTALS REPORT'
 		pw.println '----------------------'
 		
@@ -28,19 +29,42 @@ class CategoryTotalReportService {
 		pw.println ''
 		pw.println commonReportingService.horizonalRule
 		pw.println ''
+		
+		//Report by Amount 
+		pw.println 'CATEGORY TOTALS REPORT (Largest to Smallest amount)'
+		pw.println '---------------------------------------------------'
+		
+		String query2 = 'SELECT CATEGORY as TOTAL_NAME, SUM(AMOUNT) as AMT FROM FINANCIAL GROUP BY CATEGORY ORDER BY 2 DESC'
+		println query2
+		println ''
+		
+		List<TotalDTO> totalList2 = databaseQueryService.buildTotalsList(query2)
+		
+		report(totalList2, pw)
+		
+		
+		pw.println ''
+		pw.println '\t(See \'category-detail-report.txt\' for specific details)'
+		
+		pw.println ''
+		pw.println commonReportingService.horizonalRule
+		pw.println ''
 	}
 	
 	def report(List<TotalDTO> totalList, PrintWriter pw) {
 		NumberFormat nf = NumberFormat.getCurrencyInstance()
 		
+		BigDecimal grandTotal = new BigDecimal(0.0)
+		
 		totalList.each {dto ->
-			String col1 = dto.totalName.padRight(20, ' ')
+			grandTotal = grandTotal.add(dto.amount)
+			String col1 = dto.totalName.padRight(21, ' ')
 			String col2 = nf.format(dto.amount).padLeft(12, ' ')
 			pw.println "$col1$col2"
 		}
-		
 		pw.println ''
-		pw.println '\t(See \'category-detail-report.txt\' for specific details)'
+		pw.println "Grand Total: ${nf.format(grandTotal)}"
+
 	}
 	
 }
