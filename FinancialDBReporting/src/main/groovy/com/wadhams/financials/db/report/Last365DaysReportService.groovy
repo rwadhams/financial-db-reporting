7ac03684d4c98e1cd44aa03ba882d30ffb6a81ab
robert.wadhams <robert.wadhams@gmail.com>
2021-11-10 15:06:52 +1030

    Fixed Last365DaysReportService query

--- FinancialDBReporting/src/main/groovy/com/wadhams/financials/db/report/Last365DaysReportService.groovy
+++ FinancialDBReporting/src/main/groovy/com/wadhams/financials/db/report/Last365DaysReportService.groovy
@@ -6,6 +6,7 @@ import com.wadhams.financials.db.service.DatabaseQueryService
 
 import java.text.NumberFormat
 import java.text.SimpleDateFormat
+import java.time.YearMonth
 
 class Last365DaysReportService {
 	DatabaseQueryService databaseQueryService = new DatabaseQueryService()
@@ -71,11 +72,25 @@ class Last365DaysReportService {
 	
 	String buildQuery() {
 		StringBuilder sb = new StringBuilder()
+		YearMonth now = YearMonth.now()
+		
+		YearMonth start1 = now.minusYears(1L)	//1 year
+		String start = start1.atDay(1).toString()
+		//println "start date...: $start"
+		
+		YearMonth end1 = now.minusMonths(1L)	//1 month
+		String end = end1.atEndOfMonth().toString()
+		//println "end date.....: $end"
 		
 		sb.append("SELECT TRANSACTION_DT as TXN, AMOUNT as AMT, PAYEE, DESCRIPTION as DESC, ASSET, CATEGORY as CAT, SUB_CATEGORY as SUBCAT, START_DT as START, END_DT as END, RPT_GRP_1 as RG1, RPT_GRP_2 as RG2, RPT_GRP_3 as RG3 ")
 		sb.append("FROM FINANCIAL ")
-		sb.append("WHERE TRANSACTION_DT >= (SELECT DATEADD('DAY', -365, MAX(TRANSACTION_DT)) FROM FINANCIAL) ")
-		sb.append(" ORDER BY AMOUNT DESC")
+		sb.append("WHERE TRANSACTION_DT >= '")
+		sb.append(start)
+		sb.append("' AND ")
+		sb.append("TRANSACTION_DT <= '")
+		sb.append(end)
+		sb.append("' ")
+		sb.append("ORDER BY AMOUNT DESC")
 		
 		return sb.toString()
 	}
