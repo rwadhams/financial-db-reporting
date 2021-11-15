05b5490cecfad5ee8ec6ebf3175a2119badecc82
robert.wadhams <robert.wadhams@gmail.com>
2021-11-15 16:42:22 +1030

    Added Timestamped file naming

--- FinancialDBReporting/src/main/groovy/com/wadhams/financials/db/app/FinancialReportingApp.groovy
+++ FinancialDBReporting/src/main/groovy/com/wadhams/financials/db/app/FinancialReportingApp.groovy
@@ -1,5 +1,9 @@
 package com.wadhams.financials.db.app
 
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
 import com.wadhams.financials.db.controller.BudgetReportingController
 import com.wadhams.financials.db.controller.FinancialReportingController
 import com.wadhams.financials.db.type.Run
@@ -14,13 +18,20 @@ class FinancialReportingApp {
 			println "Run parameter: $run"
 			println ''
 			if (run == Run.TimestampReport) {
-				String datetime = (new Date()).format('yyyy-MM-dd-HH-mm-ss')
-				PrintWriter pw = (new File("out/financial-report-${datetime}.txt")).newPrintWriter()
+				DateTimeFormatter suffixdtf = DateTimeFormatter.ofPattern('yyyy-MM-dd-HH-mm-ss')
+				LocalDateTime ldt = LocalDateTime.now()
+				String datetime = ldt.format(suffixdtf)
+				PrintWriter pw1 = (new File("out/financial-report-${datetime}.txt")).newPrintWriter()
 				
 				FinancialReportingController controller = new FinancialReportingController()
-				controller.execute(pw)
+				controller.execute(pw1)
 				
-				pw.close()
+				pw1.close()
+				
+				PrintWriter pw2 = (new File("out/budget-report-${datetime}.txt")).newPrintWriter()
+				BudgetReportingController controller2 = new BudgetReportingController()
+				controller2.execute(pw2)
+				pw2.close()
 			}
 			else if (run == Run.OverWriteReport) {
 				PrintWriter pw1 = (new File('out/financial-report.txt')).newPrintWriter()
