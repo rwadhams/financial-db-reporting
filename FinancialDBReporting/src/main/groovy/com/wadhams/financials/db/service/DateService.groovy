9fbd96354e829a37cd664a2034af0be5bb867b47
robert.wadhams <robert.wadhams@gmail.com>
2026-09-17 12:56:40 +1000

    Added minTransactionDate, Added formatted year-month-day for a Period

--- FinancialDBReporting/src/main/groovy/com/wadhams/financials/db/service/DateService.groovy
+++ FinancialDBReporting/src/main/groovy/com/wadhams/financials/db/service/DateService.groovy
@@ -1,6 +1,7 @@
 package com.wadhams.financials.db.service
 
 import java.time.LocalDate
+import java.time.Period
 import java.time.YearMonth
 import java.time.format.DateTimeFormatter
 
@@ -16,6 +17,7 @@ class DateService {
 	LocalDate campHillSaleDate
 	LocalDate fingalPurchaseDate
 	
+	LocalDate minTransactionDate
 	LocalDate maxTransactionDate
 	YearMonth latestYearMonth
 	List<YearMonth> previous12YearMonthList
@@ -25,11 +27,23 @@ class DateService {
 		campHillSaleDate = LocalDate.of(2020, 9, 24)
 		fingalPurchaseDate = LocalDate.of(2023, 3, 2)
 		
+		minTransactionDate = findMinTransactionDate()
 		maxTransactionDate = findMaxTransactionDate()
 		latestYearMonth = YearMonth.from(maxTransactionDate)
 		previous12YearMonthList = buildPrevious12YearMonthList()
 	}
 
+	LocalDate findMinTransactionDate() {
+		String query = "select min(transaction_dt) as MD from financial"
+		//println query
+		//println ''
+
+		GroovyRowResult grr = sql.firstRow(query)
+		Date minDate = grr.getProperty('MD')
+
+		return LocalDate.parse(minDate.toString(), h2DTF)
+	}
+	
 	LocalDate findMaxTransactionDate() {
 		//String query = "select max(transaction_dt) as MD from financial where transaction_dt <= '${LocalDate.now().format(h2DTF)}' and (rpt_grp_2 is null or rpt_grp_2 <> 'FUTURE')"
 		String query = "select max(transaction_dt) as MD from financial where transaction_dt <= '${LocalDate.now().format(h2DTF)}' and rpt_grp_2 is null"
@@ -49,6 +63,7 @@ class DateService {
 		println "\tfingalPurchaseDate...: $fingalPurchaseDate"
 		println ''
 		
+		println "minTransactionDate...........: $minTransactionDate"
 		println "maxTransactionDate...........: $maxTransactionDate"
 		println "latestYearMonth..............: $latestYearMonth"
 		println "previous12YearMonthList......: $previous12YearMonthList"
@@ -62,5 +77,14 @@ class DateService {
 		}
 
 		return previous12List.sort()
+	}
+	
+	//TODO only works for greater than 365 days
+	String formatPeriodBetweenDates(LocalDate startDate, LocalDate endDate) {
+		Period period = Period.between(startDate, endDate);
+		int pYears = period.getYears()
+		int pMonths = period.getMonths()
+		int pDays = period.getDays()
+		return "$pYears years, $pMonths months, $pDays days"
 	}
 }
